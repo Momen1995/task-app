3e8ab9b909ef19cd10ef609112c43ea8bb7075e8
Momen1995 <momensrk@gmail.com>
2025-04-22 20:11:50 +0600

    some update in select item

--- src/components/TaskBoards/AllTasks.jsx
+++ src/components/TaskBoards/AllTasks.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 const AllTasks = () => {
+  const [selectTasks, setSelectTasks] = useState("");
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <>
-      <select className="w-full md:w-3/12 border border-gray-300 p-2 rounded-md text-sm md:text-base focus:outline-none focus:ring-2 focus:ring-red-400 transition">
+      <select
+        className="w-full md:w-3/12 border border-gray-300 p-2 rounded-md text-sm md:text-base focus:outline-none focus:ring-2 focus:ring-red-400 transition"
+        value={selectTasks}
+        onChange={handleChange} 
+      >
         <option value="">All Tasks</option>
-        <option value="Priority">Priority</option>
-        <option value="Due Date">Due Date</option>
-        <option value="Status">Status</option>
+        <option value="Todo">ToDo</option>
+        <option value="Inprogress">Inprogress</option>
+        <option value="Review">Review</option>
+        <option value="Done">Done</option>
       </select>
     </>
   );
